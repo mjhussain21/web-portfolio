51968e906fcbfc7e9b8acec9c3dc86ab0eec90f2
Jamil <mohammadhussain95@googlemail.com>
2018-08-11 21:01:40 +0100

    modified menu button styles and included js to menu button classes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,58 @@
-console.log('hello world again')
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+//initial menu state
+//true = open;
+//false = closed ;
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+
+  menuBtn.classList.toggle('close');
+  menu.classList.toggle('show');
+  menuBranding.classList.toggle('show');
+  menuNav.classList.toggle('show');
+
+  //loop through Nav Items
+  navItems.forEach(item => item.classList.toggle('show'));
+
+
+
+  // //if overlay is not shown
+  // if (!showMenu) {
+  //   menuBtn.classList.add('close');
+  //   menu.classList.add('show');
+  //   menuBranding.classList.add('show');
+  //   menuNav.classList.add('show');
+
+  //   //loop through Nav Items 
+  //   //navItems.forEach(item => item.classList.add('show'));
+
+  //   navItems.forEach(function (item) {
+  //     item.classList.add('show');
+  //   });
+
+  //   showMenu = true;
+
+  // } else {
+  //   menuBtn.classList.remove('close');
+  //   menu.classList.remove('show');
+  //   menuBranding.classList.remove('show');
+  //   menuNav.classList.remove('show');
+
+  //   //loop through Nav Items 
+  //   navItems.forEach(function (item) {
+  //     item.classList.remove('show');
+  //   });
+
+
+  //   showMenu = false;
+  // }
+}
